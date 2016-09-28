68946db819c46b63978d6c0fbc8f6f2221e2c424
Derrick <djchie.dev@gmail.com>
2016-09-27 23:20:26 -0700

    Restructures the folder structure

--- server.js
+++ server.js
@@ -5,8 +5,38 @@ var server = express();
 server.use(express.static('public'));
 
 server.get('/', function(request, response) {
-    response.sendfile(__dirname + '/public/index.html');
-}).listen(port, function () {
+    response.sendFile(__dirname + '/public/html/index.html');
+});
+
+server.get('/brothers', function(request, response) {
+    response.sendFile(__dirname + '/public/html/brothers.html');
+});
+
+server.get('/careers', function(request, response) {
+    response.sendFile(__dirname + '/public/html/careers.html');
+});
+
+server.get('/careers2014', function(request, response) {
+    response.sendFile(__dirname + '/public/html/careers2014.html');
+});
+
+server.get('/careers2015', function(request, response) {
+    response.sendFile(__dirname + '/public/html/careers2015.html');
+});
+
+server.get('/gallery', function(request, response) {
+    response.sendFile(__dirname + '/public/html/gallery.html');
+});
+
+server.get('/recruitment', function(request, response) {
+    response.sendFile(__dirname + '/public/html/recruitment.html');
+});
+
+server.get('/sponsorship', function(request, response) {
+    response.sendFile(__dirname + '/public/html/sponsorship.html');
+});
+
+server.listen(port, function () {
   console.log('Server listening on: ' + port);
   console.log('Please go to http://localhost:' + port);
 });
